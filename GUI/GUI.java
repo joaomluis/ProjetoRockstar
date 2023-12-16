6315d99a758d38ce37685e01c243201edc18ae15
joaomluis <jooluis2@gmail.com>
2023-12-16 17:35:01 +0000

    Painel Inicial conclusão

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -3,8 +3,10 @@ package GUI;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
 
 
     private JLabel labelWelcomeText;
@@ -12,6 +14,9 @@ public class GUI {
     private JPanel panel;
     private JButton logInButton;
     private JButton createAccButton;
+    private JRadioButton radioButtonClient;
+    private JRadioButton radioButtonMusician;
+
 
     public GUI() {
 
@@ -29,26 +34,89 @@ public class GUI {
         //Texto da plataforma RockStar
         labelWelcomeText = new JLabel();
         labelWelcomeText.setText("RockStar.Inc");
-        //labelWelcomeText.setHorizontalAlignment(JLabel.CENTER);
-        //labelWelcomeText.setVerticalAlignment(JLabel.TOP);
         labelWelcomeText.setForeground(Color.BLACK);
         labelWelcomeText.setBorder(new LineBorder(Color.BLACK));
-        labelWelcomeText.setFont(new Font("Arial", Font.BOLD, 30));
-        labelWelcomeText.setBounds(150, 80, 200, 40);
+        labelWelcomeText.setFont(new Font("Arial", Font.BOLD, 36));
+        labelWelcomeText.setBounds(138, 80, 223, 40);
 
+        // Botão de log in
         logInButton = new JButton();
+        logInButton.setBounds(100, 200, 115, 32);
+        logInButton.setText("Log In");
+        logInButton.setFocusable(false);
+        logInButton.setFont(new Font("Arial", Font.BOLD, 15));
+        logInButton.setForeground(Color.black);
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(radioButtonClient.isSelected()) {
+                    System.out.println("log in cliente");
+                } else if (radioButtonMusician.isSelected()) {
+                    System.out.println("log in musico");
+                }
+            }
+        });
 
+        //Botão de criar nova conta
+        createAccButton = new JButton();
+        createAccButton.setBounds(285, 200, 115, 32);
+        createAccButton.setText("Criar Conta");
+        createAccButton.setFocusable(false);
+        createAccButton.setFont(new Font("Arial", Font.BOLD, 15));
+        createAccButton.setForeground(Color.black);
+        createAccButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (radioButtonClient.isSelected()) {
+                    System.out.println("Criar conta cliente");
+                } else if (radioButtonMusician.isSelected()) {
+                    System.out.println("Criar conta músico");
+                }
+            }
+        });
 
+        ButtonGroup userGroup = new ButtonGroup();
+        //Botão seleção interface Cliente
+        radioButtonClient = new JRadioButton();
+        radioButtonClient.setBounds(160, 280, 77, 30);
+        radioButtonClient.setText("Cliente");
+        radioButtonClient.setFont(new Font("Arial", Font.BOLD, 15));
+        radioButtonClient.setForeground(Color.black);
+        radioButtonClient.setBackground(new Color(124, 98, 171));
+        radioButtonClient.setFocusable(false);
+        radioButtonClient.addActionListener(this);
+
+        //Botão seleção interface Musico
+        radioButtonMusician = new JRadioButton();
+        radioButtonMusician.setBounds(260, 280, 77, 30);
+        radioButtonMusician.setText("Músico");
+        radioButtonMusician.setFont(new Font("Arial", Font.BOLD, 15));
+        radioButtonMusician.setForeground(Color.black);
+        radioButtonMusician.setBackground(new Color(124, 98, 171));
+        radioButtonMusician.setFocusable(false);
+
+        userGroup.add(radioButtonClient);
+        userGroup.add(radioButtonMusician);
+
+        //Criação do painel e adição dos diferentes componentes 
         panel = new JPanel();
         panel.setLayout(null);
         panel.setBackground(new Color(124, 98, 171));
         panel.add(labelWelcomeText);
+        panel.add(logInButton);
+        panel.add(createAccButton);
+        panel.add(radioButtonClient);
+        panel.add(radioButtonMusician);
 
 
         frame.setVisible(true);
         frame.add(panel);
         frame.revalidate();
 
+
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
 }
